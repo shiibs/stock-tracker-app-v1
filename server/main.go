a8254c8c191dd5d21dc80aa66e178e62111198aa
Saha-Shibani <sahashibani57@gmail.com>
2024-10-14 21:41:18 +0530

    added finnhub api connection and reciving message through websocket

--- server/main.go
+++ server/main.go
@@ -1,5 +1,123 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"gorm.io/gorm"
+)
+
+var (
+	symbols = []string{"AAPL", "AMZN", "TSLA", "GOOGL", "NFLX", "PYPL"}
+
+	broadcast = make(chan *BroadcastMessage)
+
+	tempCandles = make(map[string]*TempCandle)
+
+	mu sync.Mutex
+)
+
 func main() {
 	env := EnvConfig()
+
+	db := DBConn(env)
+
+	finnhubWSConn := connectToFinnhub(env)
+	defer finnhubWSConn.Close()
+
+	go handleFinnhubMessage(finnhubWSConn, db)
+
+}
+
+// Connect to finnhub websockets
+func connectToFinnhub(env *Env) *websocket.Conn {
+	ws, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("wss://ws.finnhub.io?token=%s", env.API_KEY), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, s := range symbols {
+		msg, _ := json.Marshal(map[string]interface{}{"type": "subscribe", "symbol": s})
+		ws.WriteMessage(websocket.TextMessage, msg)
+	}
+
+	return ws
+}
+
+func handleFinnhubMessage(ws *websocket.Conn, db *gorm.DB) {
+	finnhubMessage := &FinnhubMessage{}
+
+	for {
+		if err := ws.ReadJSON(finnhubMessage); err != nil {
+			fmt.Println("Error reading message: ", err)
+			continue
+		}
+
+		if finnhubMessage.Type == "trade" {
+			for _, trade := range finnhubMessage.Data {
+				processTradeData(&trade, db)
+			}
+		}
+
+		cutOffTime := time.Now().Add(-20 * time.Minute)
+		db.Where("timestamp < ?", cutOffTime).Delete(&Candle{})
+	}
+}
+
+func processTradeData(trade *TradeData, db *gorm.DB) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	symbol := trade.Symbol
+	price := trade.Price
+	volume := float64(trade.Volume)
+	timestamp := time.UnixMilli(trade.Timestamp)
+
+	tempCandle, exists := tempCandles[symbol]
+
+	if !exists || timestamp.After(tempCandle.CloseTime) {
+		if exists {
+			candle := tempCandle.toCandle()
+
+			if err := db.Create(candle).Error; err != nil {
+				fmt.Println("Error saving the candle to DB: ", err)
+			}
+
+			broadcast <- &BroadcastMessage{
+				UpdateType: Closed,
+				Candle:     candle,
+			}
+		}
+
+		tempCandle = &TempCandle{
+			Symbol:     symbol,
+			OpenTime:   timestamp,
+			CloseTime:  timestamp.Add(time.Minute),
+			OpenPrice:  price,
+			ClosePrice: price,
+			HighPrice:  price,
+			LowPrice:   price,
+			Volume:     volume,
+		}
+	}
+
+	tempCandle.ClosePrice = price
+	tempCandle.Volume += volume
+	if price > tempCandle.HighPrice {
+		tempCandle.HighPrice = price
+	}
+
+	if price < tempCandle.LowPrice {
+		tempCandle.LowPrice = price
+	}
+
+	tempCandles[symbol] = tempCandle
+
+	broadcast <- &BroadcastMessage{
+		UpdateType: Live,
+		Candle:     tempCandle.toCandle(),
+	}
 }
--- server/models.go
+++ server/models.go
@@ -1,4 +1,60 @@
 package main
 
+import "time"
+
+// Candle struct represents a single OHLC (High, Low, Open, Close) candle
 type Candle struct {
+	Symbol    string    `json:"symbol"`
+	Open      float64   `json:"open"`
+	Close     float64   `json:"close"`
+	High      float64   `json:"high"`
+	Low       float64   `json:"low"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type FinnhubMessage struct {
+	Data []TradeData `json:"data"`
+	Type string      `json:"type"`
+}
+
+type TradeData struct {
+	Close     []string `json:"c"`
+	Price     float64  `json:"p"`
+	Symbol    string   `json:"s"`
+	Timestamp int64    `json:"t"`
+	Volume    int      `json:"v"`
+}
+
+type TempCandle struct {
+	Symbol     string
+	OpenTime   time.Time
+	CloseTime  time.Time
+	OpenPrice  float64
+	ClosePrice float64
+	HighPrice  float64
+	LowPrice   float64
+	Volume     float64
+}
+
+type BroadcastMessage struct {
+	UpdateType UpdateType `json:"updateType"`
+	Candle     *Candle    `json:"candle"`
+}
+
+type UpdateType string
+
+const (
+	Live   UpdateType = "live"
+	Closed UpdateType = "closed"
+)
+
+func (tc *TempCandle) toCandle() *Candle {
+	return &Candle{
+		Symbol:    tc.Symbol,
+		Open:      tc.OpenPrice,
+		Close:     tc.ClosePrice,
+		High:      tc.HighPrice,
+		Low:       tc.LowPrice,
+		Timestamp: tc.CloseTime,
+	}
 }
